7d1427d2d714822fc551e7f0a7b38ee3b495b4d7
AbrahaKahsay <abrahak81@gmail.com>
2022-12-09 16:14:32 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
+  # API routes
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
 end
