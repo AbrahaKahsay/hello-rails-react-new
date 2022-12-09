d8e8559450be9fb0bc4a1a1fd3fe344441b073e0
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-10 00:40:29 +0300

    Add application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+    </React.StrictMode>
+);
