2510d32541ad14bb6bdd6a8534eea9fe70952c8d
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-10 10:45:13 +0300

    Fix rubocop issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,9 +8,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Message.destroy_all
-Message.create(greeting: "Hello World!")
-Message.create(greeting: "Give me a job!")
-Message.create(greeting: "I'm a good Man!")
-Message.create(greeting: "Some people are just too good for this world!")
-Message.create(greeting: "I'm not one of them!")
+Message.create(greeting: 'Hello beautiful people!')
+Message.create(greeting: 'Hi there!')
+Message.create(greeting: 'Peace unto you!')
+Message.create(greeting: 'Good morning there!')
+Message.create(greeting: 'Bonjour!')
 p "Created #{Message.count} messages"
