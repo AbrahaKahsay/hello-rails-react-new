e30861df31ec24f78def58c1bea7ef31a7e8d43d
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-12-10 10:43:43 +0300

    Modify App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Greeting from './Greeting';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Greeting />} />
-      </Routes>
-    </Router>
-  );
+    return (<>
+            <Greeting/>
+    </>);
 }
 
 export default App;
