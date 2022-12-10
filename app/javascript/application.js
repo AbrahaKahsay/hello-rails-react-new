1269567e7f1f1c26f9e3055825e6f42faf0de5b5
AbrahaKahsay <abrahak81@gmail.com>
2022-12-10 10:43:29 +0300

    Modify application js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,20 @@
-// Entry point for the build script in your package.json
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import { Provider} from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  configureStore from './redux/configureStore';
+import { getGreetings } from './redux/greetings/greetingsSlice';
+configureStore.dispatch(getGreetings());
+
+const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-    <Provider store={store}>
-        <App />
+    <Provider store={configureStore}>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+            </Routes>
+        </Router>
     </Provider>
-    </React.StrictMode>
 );
